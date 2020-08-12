8763d39da8b565f58ea8fe3feed3896c0a130a63
dragonitaq <68042238+dragonitaq@users.noreply.github.com>
2020-08-12 23:31:28 +0800

    Finished level 4 authentication lesson.

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
-require("dotenv").config();
+/* ~~~~~~Encryption~~~~~~
+(Good to start encryption before any code run)*/
+
+// require("dotenv").config();
+
+// const encrypt = require("mongoose-encryption"); //I tried combine this with bcrypt to provide 2 layers of encryption for our database and it worked. But I don't how is it good in practice.
+
+// const md5 = require("md5"); //We don't use in this file.
+
+const bcrypt = require("bcryptjs"); //Please note this file use async encryption
+
+const saltRounds = 10; //After some searching, I think it is good to add 1 round for each proceeding year. Just my opition. It all goes down to the time factor.
+
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 app.listen(3000);
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
-
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRECT, encryptedFields: ["password"] }); //Must create this plugin before create new mongoose.model because we want to pass the encrypted schema into the new model.
+// userSchema.plugin(encrypt, { secret: process.env.SECRECT, encryptedFields: ["password"] }); //Must create this plugin before create new mongoose.model because we want to pass the encrypted schema into the new model.
 
 const User = new mongoose.model("User", userSchema);
 
@@ -32,16 +42,18 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password,
-  });
-  newUser.save(function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
@@ -53,11 +65,13 @@ app.post("/login", function (req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {
-          res.send("Wrong password.");
-        }
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render("secrets");
+          } else {
+            res.send("Wrong password.");
+          }
+        });
       } else {
         res.send("Email not registered.");
       }
